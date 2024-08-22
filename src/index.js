16eb461dc463afd1bb5573cf374b29eeb00eefb1
Aarush Maddula <100654110+AarushMaddula@users.noreply.github.com>
2024-08-21 21:28:56 -0400

    Add playGame function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const choices = ["rock", "paper", "scissors"];
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
 
@@ -13,24 +10,38 @@ function getHumanChoice() {
     return prompt("Pick Rock, Paper, or Scissors: ");
 }
 
-function playRound(humanChoice, computerChoice) {
-    let formatedHumanChoice = humanChoice.toLowerCase().trim();
-    let roundString = "";
 
-    if (formatedHumanChoice === computerChoice) roundString = `Tie! ${formatedHumanChoice} ties with ${computerChoice}`;
-    else if (
-        formatedHumanChoice === "rock" && computerChoice === "scissors" ||
-        formatedHumanChoice === "paper" && computerChoice === "rock" ||
-        formatedHumanChoice === "scissors" && computerChoice === "paper"
-    ) {
-        roundString = `You Win! ${formatedHumanChoice} beats ${computerChoice}`;
-        humanScore++
-    } else {
-        roundString = `You Lose! ${computerChoice} beats ${formatedHumanChoice}`;
-        computerScore++;
-    } 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    alert(roundString);
+    function playRound(humanChoice, computerChoice) {
+        let formatedHumanChoice = humanChoice.toLowerCase().trim();
+        let roundString = "";
+    
+        if (formatedHumanChoice === computerChoice) roundString = `Tie! ${formatedHumanChoice} ties with ${computerChoice}`;
+        else if (
+            formatedHumanChoice === "rock" && computerChoice === "scissors" ||
+            formatedHumanChoice === "paper" && computerChoice === "rock" ||
+            formatedHumanChoice === "scissors" && computerChoice === "paper"
+        ) {
+            roundString = `You Win! ${formatedHumanChoice} beats ${computerChoice}`;
+            humanScore++
+        } else {
+            roundString = `You Lose! ${computerChoice} beats ${formatedHumanChoice}`;
+            computerScore++;
+        } 
+    
+        alert(roundString);
+    }
+
+    for (i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    console.log(`Player score: ${humanScore} | Computer score: ${computerScore}`);
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+
+playGame();
+
